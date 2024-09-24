a1d64acf3a0ea370ed299623af5127407b3594c9
Kenny Adetutu <kenny.adetutu@gmail.com>
2024-09-24 16:35:15 -0400

    committing new changes

--- variables.tf
+++ variables.tf
@@ -3,8 +3,12 @@ variable "resource_group_location" {
   description = "Location of the resource group."
 }
 
+variable "resource_group_name" {
+  default     = "rg_sb_eastus_231697_1_172720426184"
+  description = "Name of the resource group."
+}
 variable "prefix" {
   type        = string
-  default     = "win-"
+  default     = "win"
   description = "Prefix of the vm resource name"
 }
